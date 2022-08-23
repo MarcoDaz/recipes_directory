140cfc4c541e0f0db1166cd5403fe48272acc85e
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-08-23 16:55:39 +0100

    setup #all test

--- lib/recipe_repository.rb
+++ lib/recipe_repository.rb
@@ -1,5 +1,22 @@
 require_relative 'recipe'
 
 class RecipeRepository
+  def all
+    sql = 'SELECT * FROM recipes;'
+    result = DatabaseConnection.exec_params(sql, [])
+    recipes = []
+    result.each do |record|
+      recipe = Recipe.new
 
+      recipe.id = record['id']
+      recipe.name = record['name']
+      recipe.cooking_time = record['cooking_time']
+      recipe.rating = record['rating']
+
+      recipes << recipe
+      
+    end
+
+    return recipes
+  end
 end
--- spec/recipe_repository_spec.rb
+++ spec/recipe_repository_spec.rb
@@ -1,22 +1,32 @@
 require 'recipe_repository'
 
-RSoec.describe RecipeRepository do
-    describe '#all' do
-        it 'displays all recipes' do
-            repo = RecipeRepository.new
-            recipes = repo.all
-
-            expect(recipes.length).to eq 2
-
-            expect(recipes[0].id).to eq 1
-            expect(recipes[0].name).to eq 'Lasagna'
-            expect(recipes[0].cooking_time).to eq 5
-            expect(recipes[0].rating).to eq 4
-
-            expect(recipes[1].id).to eq 2
-            expect(recipes[1].name).to eq 'Spaghetti Carbonara'
-            expect(recipes[1].cooking_time).to eq 30
-            expect(recipes[1].rating).to eq 3
-        end
+RSpec.describe RecipeRepository do
+  describe '#all' do
+    def reset_recipes_table
+      seed_sql = File.read('spec/seeds_recipes.sql')
+      connection = PG.connect({ host: '127.0.0.1', dbname: 'recipes_directory_test' })
+      connection.exec(seed_sql)
     end
+  
+    before(:each) do 
+      reset_recipes_table
+    end
+
+    it 'displays all recipes' do
+      repo = RecipeRepository.new
+      recipes = repo.all
+
+      expect(recipes.length).to eq 2
+
+      expect(recipes[0].id).to eq 1
+      expect(recipes[0].name).to eq 'Lasagna'
+      expect(recipes[0].cooking_time).to eq 5
+      expect(recipes[0].rating).to eq 4
+
+      expect(recipes[1].id).to eq 2
+      expect(recipes[1].name).to eq 'Spaghetti Carbonara'
+      expect(recipes[1].cooking_time).to eq 30
+      expect(recipes[1].rating).to eq 3
+    end
+  end
 end
